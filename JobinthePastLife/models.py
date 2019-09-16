324a2494573e0f258deaa06bb5fdf021cc874a78
HyejinYang <yang94lol@naver.com>
2019-09-16 09:27:29 +0900

    Made multiple 1:N models

--- JobinthePastLife/models.py
+++ JobinthePastLife/models.py
@@ -4,3 +4,10 @@ class PastLife(models.Model):
     name = models.CharField(max_length=4)
     job = models.CharField(max_length=20)
     birthday = models.CharField(max_length=15)
+
+# Reporter - Article 관계에서 reporter가 여러 글을 쓰므로
+# 1:N 관계임
+# reporter - name
+
+# Article(1) - Comment(N)
+# comment - content
--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Reporter - Article 관계에서 reporter가 여러 글을 쓰므로
+# 1:N 관계임
+# reporter - name
+class Reporter(models.Model):
+    name = models.CharField(max_length=10)
+
+
 class Article(models.Model):
     title = models.CharField(max_length = 30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    reporter = models.ForeignKey(Reporter, on_delete=models.PROTECT)
+
+# Article(1) - Comment(N)
+# comment - content
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
