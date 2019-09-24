e6eb5db2ed7f05a4a726db684f8b53a2afc002b0
HyejinYang <yang94lol@naver.com>
2019-09-24 18:02:51 +0900

    SQL

--- reboot/urls.py
+++ reboot/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
+    path('users/', include('users.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
