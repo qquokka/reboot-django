8ba3d1f2d291b58036e0b0842e0ff6a4ec4fd777
양혜진 <yang94lol@naver.com>
2019-10-23 17:49:08 +0900

    20191023

--- manytomany/models.py
+++ manytomany/models.py
@@ -8,10 +8,9 @@ class Patient(models.Model):
     doctors = models.ManyToManyField(Doctor, related_name='patients')
     # related_name으로 역참조 이름을 반드시 설정해줘야 충돌을 막을 수 있다.
 
-# class Reservation(models.Model):
-#     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-#     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+class Reservation(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f'{self.pk} 예약: {self.doctor.name}의 환자 {self.patient.name}'
-
+    def __str__(self):
+        return f'{self.pk} 예약: {self.doctor.name}의 환자 {self.patient.name}'
